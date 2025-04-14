8ba3d7c2702e6931594ac5627642f332ba69a1a9
Yakhyobek1997 <yahyobek19979797@gmail.com>
2025-04-14 23:19:13 +0900

    Feat: Created and fixed

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './app/MaterialTheme';
+import "./css/index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+      <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
